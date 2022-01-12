73db702bd93a856c858aaaa041106b4f830787f5
PasanAbeysekara <69195287+PasanAbeysekara@users.noreply.github.com>
2022-01-12 22:07:08 +0530

    binary search

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,11 @@
 package com.company;
 
-import com.company.searching.LinearSearch;
+
+import com.company.searching.BinarySearch;
 
 public class Main {
 
     public static void main(String[] args) {
-        LinearSearch.machine();
+        BinarySearch.machine();
     }
 }
--- src/com/company/searching/BinarySearch.java
+++ src/com/company/searching/BinarySearch.java
@@ -2,7 +2,29 @@ package com.company.searching;
 
 // to use binary search list should be sorted.
 public class BinarySearch {
-    public static void machine(){
 
+    private static int binarySearch(int[] numbers,int toFind){
+        int low = 0;
+        int high = numbers.length-1;
+        while(low<=high){
+            int k=(low+high)/2;
+            if(numbers[k]>toFind){
+                high = k-1;
+            }
+            else if(numbers[k]<toFind){
+                low = k+1;
+            }
+            else{
+                return k;
+            }
+        }
+        return -1;
+    }
+
+    public static void machine(){
+        int[] numbers = {0,1,2,3,4,5,6,17,19,20};
+        int toFind = 17;
+        int index = binarySearch(numbers,toFind);
+        System.out.println(index);
     }
 }
